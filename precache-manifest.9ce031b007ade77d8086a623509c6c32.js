122bd40788cf8475ffa28d7113edd8717ddfc29f
GitRafaelZamora <RafaelZamora.Me@gmail.com>
2019-07-23 15:40:12 -0700

    Updates

---
+++ precache-manifest.9ce031b007ade77d8086a623509c6c32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e7667761867c6aee9f51ccfc26e3d2",
+    "url": "/ChatFam/index.html"
+  },
+  {
+    "revision": "8f19a89c22ee27a76c70",
+    "url": "/ChatFam/static/css/main.d5305779.chunk.css"
+  },
+  {
+    "revision": "cd0f4d1644a13bd52a5d",
+    "url": "/ChatFam/static/js/2.65c926fd.chunk.js"
+  },
+  {
+    "revision": "8f19a89c22ee27a76c70",
+    "url": "/ChatFam/static/js/main.eadff1fc.chunk.js"
+  },
+  {
+    "revision": "336bf0addf29fabadd30",
+    "url": "/ChatFam/static/js/runtime~main.78a6abfa.js"
+  },
+  {
+    "revision": "ab4bb91265fac434fa21b3407b6a3e69",
+    "url": "/ChatFam/static/media/dragon.ab4bb912.png"
+  },
+  {
+    "revision": "46ab975499c044fbdb63497c98dcbf27",
+    "url": "/ChatFam/static/media/goblin.46ab9754.png"
+  }
+]);
